829636c39848725cecebc6c46f779101f8cbf386
EuanRamsay <euancramsay@aol.com>
2016-11-29 19:37:44 +0000

    Product and basket have name tests passed

---
+++ basket.js
@@ -0,0 +1,10 @@
+var basket = require("./basket");
+
+var basket = {
+ customerName: "Jimmy White",
+ loyaltyCard: true,
+ customerBasket: [],
+ finalCost: 0
+};
+
+module.exports = basket;
---
+++ product.js
@@ -0,0 +1,9 @@
+var product = require("./product");
+
+var product = {
+ name: "toaster",
+ cost: 29.95,
+ bogof: true
+};
+
+module.exports = product;
